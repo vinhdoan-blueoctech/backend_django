24a78dbb5c50c86f3a2522fca40914bad3f77111
vinh doan ngoc <doanngocvinh1@gmail.com>
2024-08-14 10:42:44 +0700

    fix: remove secret key

--- eco/settings.py
+++ eco/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-23^-f+9gwr(31&kwnay(tsh#plk!2fa%d606wa4+d@ktc4a+f8"
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,6 +80,7 @@ WSGI_APPLICATION = "eco.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# CHANGE IF NEEDED
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
