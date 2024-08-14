80a3779de9d73ed5ff25689420785c7bca30d119
vinh doan ngoc <doanngocvinh1@gmail.com>
2024-08-14 09:50:13 +0700

    fix: handle null case

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework.decorators import action
@@ -33,7 +34,10 @@ class RoleDetail(viewsets.ModelViewSet):
     @action(detail=True, methods=["post"])
     def add_permission(self, request, pk=None):
         role = self.get_object()
-        permission = Permission.objects.get(id=request.data["permission_id"])
+        permission_id = request.data.get("permission_id")
+
+        permission = get_object_or_404(Permission, id=permission_id)
+
         role.permissions.add(permission)
         role.save()
         return Response({"status": "permission added"})
@@ -41,10 +45,19 @@ class RoleDetail(viewsets.ModelViewSet):
     @action(detail=True, methods=["post"])
     def remove_permission(self, request, pk=None):
         role = self.get_object()
-        permission = Permission.objects.get(id=request.data["permission_id"])
+        permission_id = request.data.get("permission_id")
+
+        permission = get_object_or_404(Permission, id=permission_id)
+        if not role.permissions.filter(id=permission.id).exists():
+            return Response(
+                {"error": "The role does not have this permission."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         role.permissions.remove(permission)
         role.save()
-        return Response({"status": "permission removed"})
+
+        return Response({"status": "Permission removed successfully."})
 
 
 class PersonDetail(viewsets.ModelViewSet):
@@ -54,7 +67,10 @@ class PersonDetail(viewsets.ModelViewSet):
     @action(detail=True, methods=["post"])
     def add_role(self, request, pk=None):
         person = self.get_object()
-        role = Role.objects.get(id=request.data["role_id"])
+        role_id = request.data.get("role_id")
+
+        role = get_object_or_404(Role, id=role_id)
+
         person.roles.add(role)
         person.save()
         return Response({"status": "role added"})
@@ -62,7 +78,16 @@ class PersonDetail(viewsets.ModelViewSet):
     @action(detail=True, methods=["post"])
     def remove_role(self, request, pk=None):
         person = self.get_object()
-        role = Role.objects.get(id=request.data["role_id"])
+        role_id = request.data.get("role_id")
+
+        role = get_object_or_404(Role, id=role_id)
+
+        if not person.roles.filter(id=role.id).exists():
+            return Response(
+                {"error": "The person does not have this role."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         person.roles.remove(role)
         person.save()
         return Response({"status": "role removed"})
